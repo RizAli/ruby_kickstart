11882de3d5b27a4103ce38b60680187d29d00533
Rizwan Ali <rizcube@gmail.com>
2015-01-25 16:18:09 +0000

    completed challenge 4

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -7,15 +7,18 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
-def get_squares(num)
-	array = num
-	array.sort -
-	print array
-	
+def get_squares(array)
+	to_return = []
+	sorted = array.sort
+	sorted.each do |number|
+		to_return << number if sorted.include?(number*number)
+
+	end
+	return to_return
 end
 
 
-get_squares [9]                      # => []
-#get_squares [9,3]                    # => [3]
-#get_squares [9,3,81]                 # => [3, 9]
-get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
+puts get_squares [9]                      # => []
+puts get_squares [9,3]                    # => [3]
+puts get_squares [9,3,81]                 # => [3, 9]
+puts get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
